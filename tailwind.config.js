ce0d7433526312f04660ada9b3cc00d9c6d65abd
Renzo Damian <rdamian@bevertec.com>
2023-02-27 22:52:50 -0500

    Fix: Add components folder to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ const { fontFamily } = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
-		"./app/**/*.{js,ts,jsx,tsx}"
+		"./app/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
 		fontFamily: {
